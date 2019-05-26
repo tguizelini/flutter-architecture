1c0b425f7eb8356c2be0d819cda0a50971df101b
Tiago Guizelini <guizelini@guizelini.local>
2019-05-26 16:20:37 -0300

    remove routes

--- lib/app.dart
+++ lib/app.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'values/strings.dart' as strings;
 import 'package:flutter_architecture/values/theme.dart' as appTheme;
 
-//import 'package:flutter_architecture/routes.dart';
-
 import 'screens/login/index.dart';
 
 class App extends StatelessWidget {
