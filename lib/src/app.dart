8233240f067d5cf2eb65621f314f62cc10dd3c96
Tiago Guizelini <guizelini@guizelini.local>
2019-05-27 14:02:59 -0300

    nomeclatura da page, desaclopando as partials

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'values/strings.dart' as strings;
 import 'package:flutter_architecture/src/values/theme.dart' as appTheme;
 
-import 'package:flutter_architecture/src/pages/login/login_page.dart';
+import 'package:flutter_architecture/src/pages/login/index.dart';
 
 class App extends StatelessWidget {
   @override
