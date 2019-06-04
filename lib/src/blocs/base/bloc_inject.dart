8e66c341023d3716d01addde57852bbbc084aede
Tiago Guizelini <tiago.guizelini@7comm.com.br>
2019-06-04 16:16:01 -0300

    bloc inject

--- lib/src/blocs/base/bloc_inject.dart
+++ lib/src/blocs/base/bloc_inject.dart
@@ -1,13 +1,10 @@
-import 'package:flutter_architecture/src/blocs/intimacao_bloc.dart';
 import 'package:flutter_architecture/src/blocs/login_bloc.dart';
 import 'package:flutter_architecture/src/blocs/nav_bottom_bloc.dart';
 
 class BlocInject {
   LoginBloc _login = LoginBloc();
-  IntimacaoBloc _intimacao = IntimacaoBloc();
   NavBottomBloc _navBottomBar = NavBottomBloc();
 
   LoginBloc get login => _login;
-  IntimacaoBloc get intimacao => _intimacao;
   NavBottomBloc get navBottomBar => _navBottomBar;
 }
