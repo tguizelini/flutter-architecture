ee5cc2d8686f26c465f3535915d8f15ebfd80f07
Tiago Guizelini <guizelini@guizelini.local>
2019-06-13 14:59:09 -0300

    correção no helper httpHelper para fazer o intercept na instanciaçao do Dio

--- lib/src/helpers/http_helper.dart
+++ lib/src/helpers/http_helper.dart
@@ -1,67 +1,69 @@
-import 'package:flutter_architecture/src/helpers/storage_helper.dart';
-import 'package:flutter_architecture/src/services/base_url.dart';
-import 'package:flutter_architecture/src/utils/storage_keys.dart' as StorageKeys;
+import 'package:aasp_gerenciador/src/helpers/storage_helper.dart';
+import 'package:aasp_gerenciador/src/services/base/base_url.dart' as BASE_URL;
+import 'package:aasp_gerenciador/src/utils/storage_keys.dart' as StorageKeys;
 import 'package:dio/dio.dart';
 
 class HttpHelper {
   static Dio _client;
 
-  static Dio _initInstance({ bool isAuth }) {
-    if (_client == null) {
-      _client = Dio();
-      _setHeader(isAuth);
-      return _client;
-    }
+  static Future<Dio> _getInstance({ bool isAuth }) async {
+    if (_client == null) _client = Dio();
 
-    _setHeader(isAuth);
-    return _client;
-  }
-
-  static void _setHeader(bool isAuth) async {
     if (isAuth == true) {
       _client.options.headers = {
         "Content-Type": "x-www-form-urlencoded"
       };
-    } else {
-      final token = await StorageHelper.get(StorageKeys.token);
 
-      _client.options.headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $token'
-      };
+      return _client;
     }
-  }
 
-  static Future<Response> auth(String login, String senha) async {
-    final String url = BaseUrl.urlAuth;
 
-    _initInstance(isAuth: true);
-
-    final payload = {
-      "login": login,
-      "senha": senha,
+    final token = await StorageHelper.get(StorageKeys.token);
+    final cookie = await StorageHelper.get(StorageKeys.cookie);
+    
+    _client.options.headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token'
     };
 
-    return _client.post(url, data: payload);
+    return _client;
+  }
+
+  static Future<Response> authSSO(String codigo, String senha, String escritorio) async {
+    final String url = BASE_URL.authSSO;
+
+    final instance = await _getInstance(isAuth: true);
+
+    FormData payload = new FormData.from({
+      "grant_type": "password",
+      "scope": "openid profile servicointimacoes intimacoesapi",
+      "username": codigo,
+      "password": senha,
+      "office_code": escritorio,
+      "client_id": "app_gerenciador",
+      "client_secret": "secret"
+    });
+
+    return instance.post(url, data: payload);
   }
 
   static Future<Response> get(String url) async {
-    _initInstance();
-    return _client.get(url);
+    final instance = await _getInstance();
+    return instance.get(url);
   }
 
   static Future<Response> post(String url, Map<String, dynamic> body) async {
-    _initInstance();
-    return _client.post(url, data: body);
+    final instance = await _getInstance();
+    return instance.post(url, data: body);
   } 
 
   static Future<Response> put(String url, Map<String, dynamic> body) async {
-    _initInstance();
-    return _client.put(url, data: body);
+    final instance = await _getInstance();
+    return instance.put(url, data: body);
   } 
 
   static Future<Response> delete(String url, Map<String, dynamic> body) async {
-    _initInstance();
-    return _client.delete(url);
+    final instance = await _getInstance();
+    return instance.delete(url);
   } 
 }
