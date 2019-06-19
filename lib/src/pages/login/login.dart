6d89fce972c772805375d25ab9ae7b1423b85c59
Tiago Guizelini <guizelini@guizelini.local>
2019-06-19 12:10:42 -0300

    example bloc real for login

--- lib/src/pages/login/login.dart
+++ lib/src/pages/login/login.dart
@@ -1,26 +1,35 @@
 import 'package:flutter_architecture/src/blocs/base/bloc_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_architecture/src/custom-widgets/button.dart';
+import 'package:flutter_architecture/src/blocs/login_bloc.dart';
 import 'package:flutter_architecture/src/custom-widgets/container_corner.dart';
 import 'package:flutter_architecture/src/custom-widgets/container_with_margin.dart';
-import 'package:flutter_architecture/src/custom-widgets/divider_input.dart';
-import 'package:flutter_architecture/src/custom-widgets/edit_text.dart';
 import 'package:flutter_architecture/src/custom-widgets/loading.dart';
-import 'package:flutter_architecture/src/custom-widgets/logo.dart';
-import 'package:flutter_architecture/src/custom-widgets/text.dart';
-
-import 'package:flutter_architecture/src/pages/contact/contact.dart';
 import 'package:flutter_architecture/src/pages/login/login_footer.dart';
 import 'package:flutter_architecture/src/pages/login/login_form.dart';
 import 'package:flutter_architecture/src/pages/login/login_header.dart';
 
-import 'package:flutter_architecture/src/utils/navigation/nav_slide_from_top.dart';
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
 
-class LoginPage extends StatelessWidget {
+class _LoginPageState extends State<LoginPage> {
+  LoginBloc bloc;
+
+  @override
+  void initState() {
+    super.initState();
+    bloc = LoginBloc();
+  }
+
+  @override
+  void dispose() {
+    bloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final bloc = BlocProvider.of(context).login;
-
     return Loading(
       message: "doing something...",
       status: bloc.loading,
